ebf57735a6193604d61d2326953c38631ee0666e
ervon-grey <136776272+ervon-grey@users.noreply.github.com>
2025-05-21 11:45:51 +0200

    Update supabase.js
    For the supabase client

--- frontend/supabase.js
+++ frontend/supabase.js
@@ -1,7 +1,7 @@
 // supabase.js
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://your-project-id.supabase.co'; // <-- paste from dashboard
-const supabaseKey = 'your-anon-key'; // <-- paste from dashboard
+const supabaseUrl = 'https://rftzusoerdrjzyapknes.supabase.co'; // <-- paste from dashboard
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJmdHp1c29lcmRyanp5YXBrbmVzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDc2NDcxNzgsImV4cCI6MjA2MzIyMzE3OH0.igRjZrvBKWVPrvfxtPSMCz99J5eUczIMSymv4UKylpM'; // <-- paste from dashboard
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
